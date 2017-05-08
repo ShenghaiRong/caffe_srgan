644e16ae0a5ef14e28facda98285d4879dc09342
rsh <1173885314@qq.com>
2017-05-08 15:52:47 +0800

    Add the Scale layer after the BN layer

--- SRGAN/train_srres-mse_74s.sh
+++ SRGAN/train_srres-mse_74s.sh
@@ -6,4 +6,4 @@
 #########################################################################
 #!/bin/bash
 DATETIME=`date +%Y-%m-%d-%R`
-./build/tools/caffe train -solver examples/SRGAN/srres-mse_74s_solver.prototxt -gpu all -snapshot examples/SRGAN/Model/SRResNet-MSE_74s_p_iter_30000.solverstate 2>&1 | tee examples/SRGAN/log/SRResNet-MSE_74s_p_${DATETIME}.txt
+./build/tools/caffe train -solver examples/SRGAN/srres-mse_74s_solver.prototxt -gpu all -weights examples/SRGAN/Model/SRResNet-MSE_74s_iter_735000.caffemodel 2>&1 | tee examples/SRGAN/log/SRResNet-MSE_74s_sb_${DATETIME}.txt
